9a84dfbf6169bd415ab823ef58e94c3e81e45c9e
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2023-03-18 15:09:12 +0000

    add dateUtils

---
+++ client/src/utils/dateUtils.js
@@ -0,0 +1,7 @@
+const formatDate = (input) => {
+    const date = new Date(input);
+
+    return date.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric'});
+}
+
+export default formatDate;
