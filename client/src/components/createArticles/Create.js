34514a9d30c16fcf764a6c50132a2f9bb1995920
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2023-03-18 15:07:26 +0000

    change folder name

--- client/src/components/createArticles/Create.js
+++ client/src/components/createArticles/Create.js
@@ -1,12 +1,14 @@
 import "./create.css";
+import { useForm } from "../../hooks/useForm";
 
 
-const Create = () => {
+const Create = ({show, onArticleCreateSubmit, onArticleCreateClose}) => {
+  const { formValues, onChangeHandler, onSubmit } = useForm({ text: ''}, onArticleCreateSubmit);
 
   return ( 
-    <div id="create">
+    <div id="create" show={show} onBlur={onArticleCreateClose}>
       <div className="container">
-        <div className="row">
+        <div className="row" closeButton onHide={onArticleCreateClose}>
           <div className="col-12 col-offset0 text-center heading">
             <h2>
               <span>Create Articles</span>
@@ -14,7 +16,7 @@ const Create = () => {
           </div>
         </div>
         <div className="col-9 row">
-          <form className="create">
+          <form className="create" onSubmit={onSubmit}>
             <div className="form-group">
               <div className="col-12 field">
                 <label htmlFor="title">Title*</label>
@@ -24,6 +26,8 @@ const Create = () => {
                   name="title"
                   placeholder="Title ..."
                   className="form-control"
+                  value={formValues.title}
+                  onChange={onChangeHandler}
                 />
               </div>
               <div className="col-12 field">
@@ -34,6 +38,8 @@ const Create = () => {
                   name="imageUrl"
                   placeholder="ImageUrl ..."
                   className="form-control"
+                  value={formValues.imageUrl}
+                  onChange={onChangeHandler}
                 />
               </div>
             </div>
@@ -46,6 +52,8 @@ const Create = () => {
                   name="category"
                   placeholder="Category ..."
                   className="form-control"
+                  value={formValues.category}
+                  onChange={onChangeHandler}
                 />
               </div>
               <div className="col-6 field">
@@ -56,6 +64,8 @@ const Create = () => {
                   name="date"
                   placeholder="Created on ..."
                   className="form-control"
+                  value={formValues.createdOn}
+                  onChange={onChangeHandler}
                 />
               </div>
             </div>
@@ -70,6 +80,8 @@ const Create = () => {
                   rows="10"
                   placeholder="Content ..."
                   className="form-control"
+                  value={formValues.content}
+                  onChange={onChangeHandler}
                 ></textarea>
               </div>
             </div>
@@ -77,6 +89,9 @@ const Create = () => {
               <p className="mandatory">* are mandatory</p>
               <button type="submit" className="btn btn-success">
                 Create
+              </button>
+              <button className="btn btn-outline" onClick={onArticleCreateClose}>
+                Cancel
               </button>
             </div>
           </form>
