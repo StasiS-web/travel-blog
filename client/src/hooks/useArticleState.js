444f7b47c25705b52d055441956dbc718420a8a0
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2023-03-27 11:40:27 +0100

    add useArticle hook

---
+++ client/src/hooks/useArticleState.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+import { useService } from "../hooks/useService";
+import { destinationServiceFactory } from "../services/destinationService";
+
+const useArticleState = (id) => {
+    const [article, setArticle] = useState({});
+    const destinationService = useService(destinationServiceFactory);
+
+    useEffect(() => {
+        destinationService.getOneArticle(id)
+            .then(article => {
+                setArticle(article)
+            })
+    }, [id]);
+
+    return [article, setArticle];
+}
+
+export default useArticleState;
